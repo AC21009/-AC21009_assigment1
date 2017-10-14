4f9b38c6f45d0a96fbfaac3e8a0c06c36d6f18fb
Douglas P <dparkinson@dundee.ac.uk>
2017-10-15 00:13:44 +0100

    This is the commit with the final logChange.sh script and some changes to the sub_menu2.sh script with added commit, delete file and view log options.

--- logChange.sh
+++ logChange.sh
@@ -1,71 +1,124 @@
 #!/bin/bashecho
 
-repo=$1
-g_repo=$1".repo_files/ghost_repo/"
-log=$repo".repo_files/log.txt"
+log=$1"/.repo_files/log.txt"
 
 #if there is no repo_files folder in the repo make on
-test -e "$repo.repo_files/" || (mkdir "$repo.repo_files/"; chmod ug+rwx "$repo.repo_files/") 
+test -e "$1/.repo_files/" || (mkdir "$1/.repo_files/"; chmod ug+rwx "$1/.repo_files/") 
 
 #Make ghost copies of the repo files if there is not a ghost repo located in the repo_files
-test  -e "$g_repo" || (mkdir "$g_repo"; chmod ug+rwx "$repo.repo_files/"; rsync -av --exclude=".*" $repo $g_repo)
+test  -e "$1/.repo_files/ghost_repo" || (rsync -av --exclude=".*" "$1" "$1/.repo_files/"; mv "$1/.repo_files/$(basename $1)" "$1/.repo_files/ghost_repo")
 
+logDirectory(){
+	#Check to see if changes have been made
+	#If there are changes note them down in a log file
+	dir=$1
 
-#Check to see if changes have been made
-#If there are changes note them down in a log file
-repo_files=$(ls $repo)
-g_repo_files=$(ls $g_repo)
-for f in $repo_files; do
+	echo $dir
+	if [ "$dir" != "" ]; then
+		rootdir=$4
+		repo="$2/$dir"
+		g_repo="$3/$dir"
+	else
+		rootdir=$2
+		repo=$2
+		g_repo="$3"
+	fi
 
-	sort "$repo$f" > "$repo.repo_files/.repoSort"
+	if [ -d $repo ]; then
+		repo_files=$(ls "$repo")
+	else
+		repo_files=""
+	fi
 
-	for f_ghost in $g_repo_files; do 
-		if [ "$f" = "$f_ghost" ];
-		then
-			sort "$g_repo$f_ghost" > "$repo.repo_files/.g_repoSort"			
-			
-			#lines added since last commit
-			comm -23 "$repo.repo_files/.repoSort" "$repo.repo_files/.g_repoSort" > "$repo.repo_files/.repoCompare"
-			while read line; do
-				i=1
-				while read l; do
-					if [ "$l" = "$line" ]; then
-						(echo "$f [+] $i: $line") >> $log
-					fi
-					i=$(($i+1))
-				done < $repo$f
-			done < "$repo.repo_files/.repoCompare"
+	if [ -d $g_repo ]; then
+		g_repo_files=$(ls "$g_repo")
+	else
+		g_repo_files=""
+	fi
 
-			#lines removed since last commit
-			comm -13 "$repo.repo_files/.repoSort" "$repo.repo_files/.g_repoSort" > "$repo.repo_files/.repoCompare"
-			while read line; do
-				i=1
-				while read l; do
-					if [ "$l" = "$line" ]; then
-						(echo "$f [-] $i: $line") >> $log
-					fi
-					i=$(($i+1))
-				done < $g_repo$f_ghost
-			done < "$repo.repo_files/.repoCompare"
+	for f in $g_repo_files; do
+
+		if [ ! -d "$g_repo_files/$f" ]; then
+			test -e "$repo/$f" || (echo "$repo/$f [+] 0: [FILE DELETED] [$f]") >> $log
 		fi
 	done
-done
+	
+	
+	for f in $repo_files; do
+		if [ ! -d "$repo/$f" ]; then
+			if [ ! -e "$g_repo/$f" ]; then
+				(echo "$repo/$f [+] 0: [FILE ADDED] [$f]") >> $log
+				i=1
+				while read line ; do
+					echo $line
+					(echo "$repo/$f [+] $i: $line [$f]") >> $log
+					i=$(($i+1))
+				done < "$repo/$f"
+			fi			
+		fi
+	done
 
-for f in $g_repo_files; do
-	test -e $repo$f || (echo "$f [+] 0: [FILE DELETED]") >> $log
-done
 
-for f in $repo_files; do
-	if [ ! -e $g_repo$f ]; then
-		(echo "$f [+] 0: [FILE ADDED]") >> $log
-		i=1
-		while read line ; do
-			echo $line
-			(echo "$f [+] $i: $line") >> $log
-			i=$(($i+1))
-		done < "$repo$f"
+	for f in $repo_files; do
+
+		if [ -d "$repo/$f" ]; then
+			(logDirectory $f $repo $g_repo $rootdir)
+			echo "Directory $repo/$f Dive Complete"
+		else
+			test ! -e "$repo/$f" || sort "$repo/$f" > "$rootdir/.repo_files/.repoSort"
+		fi
+	
+		for f_ghost in $g_repo_files; do
+
+			if [ -d "$g_repo/$f_ghost" ]; then
+				(logDirectory $f_ghost $repo $g_repo $rootdir)
+			else
+				
+				if [ "$f" = "$f_ghost" ]; then
+
+					test ! -e "$g_repo/$f_ghost" || sort "$g_repo/$f_ghost" > "$rootdir/.repo_files/.g_repoSort"			
+					
+					#lines added since last commit
+					comm -23 "$rootdir/.repo_files/.repoSort" "$rootdir/.repo_files/.g_repoSort" > "$rootdir/.repo_files/.repoCompare"
+					while read line; do
+						i=1
+						while read l; do
+							if [ "$l" = "$line" ]; then
+								(echo "$repo/$f [+] $i: $line [$f]") >> $log
+							fi
+							i=$(($i+1))
+						done < "$repo/$f"
+					done < "$rootdir/.repo_files/.repoCompare"
+		
+					#lines removed since last commit
+					comm -13 "$rootdir/.repo_files/.repoSort" "$rootdir/.repo_files/.g_repoSort" > "$rootdir/.repo_files/.repoCompare"
+					while read line; do
+						i=1
+						while read l; do
+							if [ "$l" = "$line" ]; then
+								(echo "$repo/$f [-] $i: $line [$f]") >> $log
+							fi
+							i=$(($i+1))
+						done < "$g_repo/$f_ghost"
+					done < "$rootdir/.repo_files/.repoCompare"
+				fi
+			fi
+		done
+	done
+
+	
+	if [ -d "$repo" ] || [ -d "$g_repo" ]; then
+		if [ ! -d "$g_repo" ] && [ -d "$repo" ] ; then
+			(echo "$repo [+] 0: [DIRECTORY ADDED] [$(basename $repo)]") >> $log
+		elif [ ! -d "$repo" ] && [ -d "$g_repo" ] ; then
+			(echo "$repo [+] 0: [DIRECTORY DELETED] [$(basename $repo)]") >> $log
+		fi
 	fi
-done
+}
 
-(sort -u $log) >> "$repo.repo_files/sortedLog.txt"
-mv "$repo.repo_files/sortedLog.txt" "$repo.repo_files/log.txt"
+#Main Code#
+
+logDirectory "" "$1" "$1/.repo_files/ghost_repo"
+
+(sort -u $log) >> "$1/.repo_files/sortedLog.txt"
+mv "$1/.repo_files/sortedLog.txt" "$1/.repo_files/log.txt"
--- sub_menu2.sh
+++ sub_menu2.sh
@@ -14,7 +14,7 @@ ls -1 $1
 
 read -p 'Please enter the name of the file to view: ' fileName
 clear
-less < $1$fileName
+less "$1/$fileName"
 
 sh ./sub_menu2.sh $1
 }
@@ -31,8 +31,9 @@ ls -1 $1
 
 read -p 'Please enter the name of the file to edit: ' fileName
 clear
-nano $1$fileName
+nano "$1/$fileName"
 
+logChanges $1
 sh ./sub_menu2.sh $1
 }
 
@@ -49,13 +50,50 @@ ls -1 $1
 
 read -p 'Please enter the name of the file: ' fileName
 clear
-touch < $1$fileName
+touch < "$1/$fileName"
 
+logChanges $1
 sh ./sub_menu2.sh $1
 }
 
+deleteFile(){
+cat << FILES
+-------------------------------------------------
+#####		    FILE LIST               #####
+-------------------------------------------------
+FILES
+
+clear
+ls -1 $1
+
+read -p 'Please enter the name of the file: ' fileName
+clear
+rm -R "$1/$fileName"
+
+	logChanges $1
+	sh ./sub_menu2.sh $1
+}
+
+viewLog(){
+	logChanges $1
+	less "$1/.repo_files/log.txt"
+	sh ./sub_menu2.sh $1
+}
+
+commitRepo(){
+	logChanges $1
+	rm -R "$1/.repo_files/ghost_repo"
+	rsync -av --exclude=".*" "$1" "$1/.repo_files/"
+	mv "$1/.repo_files/$(basename $1)" "$1/.repo_files/ghost_repo"
+	"####	COMMIT	####" >> "$1/.repo_files/commit_log.txt"
+	cat "$1/.repo_files/log.txt" >> "$1/.repo_files/commit_log.txt"
+	"" > "$1/.repo_files/log.txt"
+	
+	sh ./sub_menu2.sh $1
+}
+
 logChanges(){
-sh logChange.sh $1
+	sh logChange.sh $1
 }
 
 restoreBackup(){
@@ -89,6 +127,7 @@ cat << MENU
 5. View Log	
 6. Restore Backup
 7. Compile Code
+8. Commit Repo
 0. Back
 -------------------------------------------------
 MENU
@@ -101,12 +140,13 @@ ls -1 $1
 read option
 case "$option" in
 1)viewFile $1;;
-2)editFile $1; logChanges $1;;
-3)createFile $1; logChanges $1;;
-4);;
-5);;
+2)editFile $1;;
+3)createFile $1;;
+4)deleteFile $1;;
+5)viewLog $1;;
 6)restoreBackup $1;;
 7);;
+8)commitRepo $1;;
 0) clear; sh ./sub_menu1.sh ;;
 *) sh ./sub_menu2.sh $1 ;;
 esac 
